dc2f91fa496354cc019f5815940a41fe43edd49d
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-07-10 16:18:43 -0500

    Se corrijieron algunas cosas del menu, sin embargo no funcionan las opciones de leer los datos, no tengo idea de porque

--- ExamenProgreso3.c
+++ ExamenProgreso3.c
@@ -1,6 +1,8 @@
 #include <stdio.h> //Se incluye la biblioteca stdio.h y la creacion de funcion main
 float Promedio(float x, float y, float z){
-    return (x+y+z)/3;
+    float resultado;
+    resultado = (x+y+z)/3;
+    return resultado;
 }
 int main(){
     struct alumnos{
@@ -15,36 +17,42 @@ int main(){
 
     FILE *archivo;
     FILE *archivonuevo;
-    archivo = fopen("alumnos.txt","r");
+    archivo = fopen("alumnos.txt","r+");
     archivonuevo = fopen("AlumnosConPromedio.txt","a+");
     int mayor = 0;
     int opcion = 0;
 
-    printf("Menu:\n");
-    printf("\n1.Leer los datos, \n2.Escribir los datos en nuevo archivo, \n3.Salir\n");
-    printf("\nIngrese una opcion del menu: ");
-    scanf("%d", &opcion);
+    do
+    {
+        printf("Menu:\n");
+        printf("\n1.Leer los datos \n2.Escribir los datos en nuevo archivo \n3.Salir\n");
+        printf("\nIngrese una opcion del menu: ");
+        scanf("%d", &opcion);
+        getchar();
 
     switch (opcion){
-    case 1: printf("\nLos datos leidos se imprimiran a continuacion: \n");
-            while(fscanf(archivo, "%d %s %s %f %f %f", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3) != EOF){
-            printf("%d %s %s %f %f %f", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
-            fflush(archivo);
+        case 1: printf("\nLos datos leidos se imprimiran a continuacion: \n");
+            while(!feof(archivo)){
+                fscanf(archivo, "%d %s %s %f %f %f", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
+                printf("%d %s %s %f %f %f\n", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
+                fflush(archivo);
             }
-        break;
-    case 2: printf("\nSe imprimieron los datos con exito\n");
-            while(fscanf(archivo, "%d %s %s %f %f %f", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3) != EOF){
-            alumnos.promedio = Promedio(alumnos.nota1, alumnos.nota2, alumnos.nota3);
-            fprintf(archivonuevo, "%d %s %s %f %f %f", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
-            fflush(archivo);
-            }
-    case 3: printf("\nSalio con exito del programa!\n");
             break;
-    default: printf("\nIngrese una opcion valida\n");
-        break;
+        case 2: printf("\nSe imprimieron los datos con exito\n");
+            while(!feof(archivo)){
+                fscanf(archivo, "%d %s %s %f %f %f", alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
+                alumnos.promedio = Promedio(alumnos.nota1, alumnos.nota2, alumnos.nota3);
+                fprintf(archivonuevo, "%d %s %s %f %f %f %f", alumnos.numero, alumnos.NombreApellido, alumnos.Carrera, alumnos.nota1, alumnos.nota2, alumnos.nota3, alumnos.promedio);
+                fflush(archivo);
+            }
+            break;
+        case 3: printf("\nSalio con exito del programa!\n");
+            break;
+        default: printf("\nIngrese una opcion valida\n");
+            break;
     }
-
-    
+    } while (opcion!=3);
+        
     fclose(archivo);
     fclose(archivonuevo);
     

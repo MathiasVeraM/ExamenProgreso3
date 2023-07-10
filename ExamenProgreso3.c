3f0884776dc37c8bac914d761fff895fb31049fa
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-07-10 15:04:39 -0500

    Se creo la estructura alumnos y se inicio el archivo alumnos.txt para leerlo

--- ExamenProgreso3.c
+++ ExamenProgreso3.c
@@ -1,4 +1,24 @@
 #include <stdio.h> //Se incluye la biblioteca stdio.h y la creacion de funcion main
 int main(){
+    struct alumnos{
+        int numero;
+        char NombreApellido [30];
+        char Carrera [30];
+        float nota1;
+        float nota2;
+        float nota3;
+        float promedio;
+    }alumnos;
+
+    FILE *archivo;
+    archivo = fopen("alumnos.txt","r");
+
+    while(!feof(archivo)){
+        fscanf(archivo, "%d %s %s %f %f %f", &alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
+
+    }
+
+
+
     return 0;
 }

7bf36742d1d02f91e6293147569de13fbfccc52f
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-07-10 15:48:23 -0500

    Se saco el fprintf del ciclo while

--- ExamenProgreso3.c
+++ ExamenProgreso3.c
@@ -15,16 +15,18 @@ int main(){
 
     FILE *archivo, *archivonuevo;
     archivo = fopen("alumnos.txt","r");
-    archivonuevo = fopen("Alumnosinvertidos.txt","a+");
+    archivonuevo = fopen("AlumnosConPromedio.txt","a+");
     int mayor = 0;
     float promedio = 0;
+
 
     while(!feof(archivo)){
         fscanf(archivo, "%d %s %s %f %f %f", &alumnos.numero, &alumnos.NombreApellido, &alumnos.Carrera, &alumnos.nota1, &alumnos.nota2, &alumnos.nota3);
         alumnos.promedio = Promedio(alumnos.nota1,alumnos.nota2,alumnos.nota3);
-        fprintf(archivonuevo, "%d; %s; %s; %f; %f; %f; %f\n", alumnos.numero, alumnos.NombreApellido, alumnos.Carrera, alumnos.nota1, alumnos.nota2, alumnos.nota3, alumnos.promedio);
     }
 
+    fprintf(archivonuevo, "%d; %s; %s; %f; %f; %f; %f\n", alumnos.numero, alumnos.NombreApellido, alumnos.Carrera, alumnos.nota1, alumnos.nota2, alumnos.nota3, alumnos.promedio);
+    printf("\nSe imprimieron los datos con exito\n");
     fclose(archivo);
     fclose(archivonuevo);
     
